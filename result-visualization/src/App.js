62dc622e6121e81ed20277fc722607fb8005baf0
doyleh4 <60394085+doyleh4@users.noreply.github.com>
2021-04-05 16:09:33 +0100

    Cleaned up folder by deleting unused files
    In the last commit I changed alot of the components, so had some unused files. Deleted them to clean up the folder

--- result-visualization/src/App.js
+++ result-visualization/src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import Header from './header.js'
 import GraphBox from './graphs.js'
-import Options from './options.js'
-import Details from "./details.js"
-
-
 
 function App() {
   var data = require('./results.json');
